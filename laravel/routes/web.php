ac6586d48f4275bcb7b04db95e6058374e6e0f3f
Sergey-TR <s.trofimov72@gmail.com>
2021-10-07 09:22:41 +0300

    change routes and create controllers

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,30 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\CategoryController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', [IndexController::class, 'index'])->name('index.index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/about', [AboutController::class, 'index'])->name('about.index');
 
-Route::get('/about', function() {
-    return view('about');
-});
+Route::get('/news', [NewsController::class, 'index'])->name('news.index');
+Route::get('/news/add', [NewsController::class, 'create'])->name('news.create');
+Route::get('/news/{news}', [NewsController::class, 'show'])->name('news.show');
 
-Route::get('/news-list', function() {
-    return view('news-list');
-});
+Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+Route::get('/categories/{category}', [CategoryController::class, 'show'])->name('categories.show');
 
-Route::get("/news/{id} ", function ($id) {
-    return view('news', ['id' => $id]);
-});
+Route::get('/auth', function () {
+    return view('auth.index');
+})->name('auth.index');
