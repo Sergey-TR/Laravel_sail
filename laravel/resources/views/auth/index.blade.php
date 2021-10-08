c1437d32730eb76c11e3b145c8c753cd1b865f8c
Sergey-TR <s.trofimov72@gmail.com>
2021-10-08 09:44:03 +0300

    add style auth and create-news

--- laravel/resources/views/auth/index.blade.php
+++ laravel/resources/views/auth/index.blade.php
@@ -5,13 +5,23 @@
 @section('content')
     <div class="auth" style="display: flex; flex-direction: column; align-items: center;
                             padding: 15px;">
-        <h3 style="color: darkred; font-weight: bold;">LOG IN</h3>
-        <form action="#" style="display: flex; flex-direction: column;">
-            <label for="name">YOUR NAME</label>
-            <input type="text" placeholder="your name or login" name="name">
-            <label for="password">PASSWORD</label>
-            <input type="password" placeholder="password">
-            <button type="submit">LOG IN</button>
+        <h1 style="color: darkred; font-weight: bold;">LOG IN</h1>
+        <form action="#" style="display: flex; flex-direction: column; justify-content: center; align-items: center;">
+            <label style="display: flex; color: coral; font-weight: bold;
+                    width: 300px; height: 30px; margin-bottom: 10px;"
+                    for="name">YOUR NAME</label>
+            <input style="display: flex; color: coral; font-weight: bold;
+                    width: 300px; height: 40px; margin-bottom: 10px; padding-left: 10px;"
+                    type="text" placeholder="your name or login" name="name">
+            <label style="display: flex; color: coral; font-weight: bold;
+                    width: 300px; height: 30px; margin-bottom: 10px;"
+                    for="password">PASSWORD</label>
+            <input style="display: flex; color: coral; font-weight: bold;
+                    width: 300px; height: 40px; margin-bottom: 20px; padding-left: 10px"
+                    type="password" placeholder="password">
+            <button style="width: 150px; background-color: coral; color: white; height: 40px;
+                    font-weight: bold; cursor: pointer;" type="submit">LOG IN</button>
         </form>
     </div>
 @endsection
+
--- laravel/resources/views/news/create.blade.php
+++ laravel/resources/views/news/create.blade.php
@@ -4,17 +4,31 @@
 
 @section('content')
     <h1 class="text-center" style="color: darkred; font-weight: bold;">CREATE NEWS</h1>
-    <form action="#">
-        <label for="category">Select news category</label>
-        <select name="category">
-            @foreach($categories as $category)
-                <option value="{{ $category->id }}">{{ $category->title }}</option>
-            @endforeach
-        </select>
-        <label for="title">Header</label>
-        <input type="text" placeholder="header" name="title">
-        <label for="description">CONTENT</label>
-        <textarea name="description" placeholder="enter the content" rows="10"></textarea>
-        <button type="submit">ADD NEWS</button>
+    <form action="#" style="display: grid; grid-template-columns: repeat(2, 1fr); justify-content: center;">
+        <div style="display: flex; flex-direction: column; align-items: center;">
+            <label style="color: coral; font-weight: bold; font-size: 20px;
+                            margin-bottom: 10px;" for="category">Select news category</label>
+            <select name="category" style="width: 300px; height: 40px; color: coral; padding: 10px;">
+                @foreach($categories as $category)
+                    <option value="{{ $category->id }}">{{ $category->title }}</option>
+                @endforeach
+            </select>
+            <button style="width: 150px; background-color: coral; color: white; height: 40px;
+                    font-weight: bold; cursor: pointer;"
+                    type="submit">ADD NEWS</button>
+        </div>
+        <div style="display: flex; flex-direction: column; align-items: center;">
+            <label style="color: coral; font-weight: bold; font-size: 20px;
+                            margin-bottom: 10px;"
+                   for="title">Header</label>
+            <input style="width: 300px; height: 40px; color: coral; padding: 10px;"
+                   type="text" placeholder="header" name="title">
+            <label style="color: coral; font-weight: bold; font-size: 20px;
+                            margin-bottom: 10px;"
+                   for="description">CONTENT</label>
+            <textarea style="width: 300px; min-height: 40px; color: coral; padding: 10px;"
+                      name="description" placeholder="enter the content" rows="10"></textarea>
+        </div>
+
     </form>
 @endsection
